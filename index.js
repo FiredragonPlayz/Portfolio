eb8f01c55b1dca3f2f2bc23fbda767d92b23857c
FiredragonPlayz <74649972+FiredragonPlayz@users.noreply.github.com>
2021-07-09 18:46:57 +0530

    f

--- index.js
+++ index.js
@@ -91,3 +91,25 @@ for (i = 0; i < linkCollapse.length; i++) {
         rotate.classList.toggle('rotate')
     })
 }
+
+anime({
+  targets: '.row svg',
+  translateY: 10,
+  autoplay: true,
+  loop: true,
+  easing: 'easeInOutSine',
+  direction: 'alternate'
+});
+
+anime({
+  targets: '#zero',
+  translateX: 10,
+  autoplay: true,
+  loop: true,
+  easing: 'easeInOutSine',
+  direction: 'alternate',
+  scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+    rotateY: {value: '+=180', delay: 200},
+});
+
+
